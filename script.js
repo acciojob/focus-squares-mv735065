a9351e21b3c23060f016115fc93faf3500767b35
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-11-03 19:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+let boxes=document.querySelectorAll(".square");
+
+for(let i=0;i<boxes.length;i++){
+	let box=boxes[i];
+	box.addEventListener('mouseover',(event)=>{
+		for(let j=0;j<boxes.length;j++){
+			if(box!=boxes[j]) boxes[j].style.backgroundColor = '#6F4E37';
+		}
+	})
+	box.addEventListener('mouseout',(event)=>{
+		for(let j=0;j<boxes.length;j++){
+			 boxes[j].style.backgroundColor = '#E6E6FA';
+		}
+		
+	})
+	
+}
+
+
